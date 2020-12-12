e1b16080030de9adb8ea5ee3ce18d72725ee5dac
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-12-12 18:13:49 -0500

    Update SafeMath.sol
    bump

--- contracts/oz/SafeMath.sol
+++ contracts/oz/SafeMath.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.17;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
 
 library SafeMath {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
