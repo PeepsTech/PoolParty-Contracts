81f2bc1e9b265b721b24cf63eb63b19417eae9d1
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-12-12 18:12:23 -0500

    Update IAAVE.sol
    rename / add reserves fetcher

--- contracts/aave/IAAVE.sol
+++ contracts/aave/IAAVE.sol
@@ -1,6 +1,8 @@
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
 
-interface IAAVE {
+interface IAAVEDepositWithdraw {
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
     function withdraw(address token, uint256 amount, address destination) external;
+    function getReservesList() external view returns (address[] memory);
 }
