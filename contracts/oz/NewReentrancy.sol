c48b350279152f11a9d94c9f73610e7b9383833d
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-12-12 18:13:11 -0500

    Update NewReentrancy.sol
    bump

--- contracts/oz/NewReentrancy.sol
+++ contracts/oz/NewReentrancy.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.5.17;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
 
 contract ReentrancyGuard { // call wrapper for reentrancy check
     bool private _notEntered;
