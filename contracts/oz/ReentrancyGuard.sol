0b0c8a8aa4084d2f9b00a8336fed34ac5dc70e5a
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-12-12 18:25:27 -0500

    Update ReentrancyGuard.sol
    refresh

--- contracts/oz/ReentrancyGuard.sol
+++ contracts/oz/ReentrancyGuard.sol
@@ -1,23 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-contract ReentrancyGuard {
+contract ReentrancyGuard { // call wrapper for reentrancy check - see https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/ReentrancyGuard.sol
     uint256 private constant _NOT_ENTERED = 1;
     uint256 private constant _ENTERED = 2;
-
     uint256 private _status;
 
-    constructor () internal {
+    constructor() {
         _status = _NOT_ENTERED;
     }
 
     modifier nonReentrant() {
-        require(_status != _ENTERED, "reentrant call");
-
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
         _status = _ENTERED;
-        
         _;
-        
         _status = _NOT_ENTERED;
     }
 }
