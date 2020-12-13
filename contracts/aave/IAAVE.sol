66e5fb3fa2bdd403f6e3dbd8545bcdb3a2a602b0
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-12-12 20:45:01 -0500

    Update IAAVE.sol
    add function to associate underlying

--- contracts/aave/IAAVE.sol
+++ contracts/aave/IAAVE.sol
@@ -5,4 +5,5 @@ interface IAAVEDepositWithdraw {
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
     function withdraw(address token, uint256 amount, address destination) external;
     function getReservesList() external view returns (address[] memory);
+    function UNDERLYING_ASSET_ADDRESS() external view returns (address);
 }
